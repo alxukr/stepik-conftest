540cef3e9e5f07fc32bb2cf44529f0e178dbf80e
alxukr <alxnet@ukr.net>
2021-10-07 04:51:30 +0300

    add the test_items.py file to the repository

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_elements_by_xpath('//*[@class="btn btn-lg btn-primary btn-add-to-basket"]')
+    # If the "button" variable has an empty list, it will cause an error
+    assert button, 'Error! The "Add to basket" button was not found.'
