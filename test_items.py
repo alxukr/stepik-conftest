bb3ad8ae48070c9f8efb605c9fa35f06300af272
alxukr <90536450+alxukr@users.noreply.github.com>
2021-10-07 05:07:28 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -7,5 +7,5 @@ def test_add_to_basket_button(browser):
     browser.get(link)
     time.sleep(30)
     button = browser.find_elements_by_xpath('//*[@class="btn btn-lg btn-primary btn-add-to-basket"]')
-    # If the "button" variable has an empty list, it will cause an error
+    # If the "button" variable has an empty list, it will cause an error.
     assert button, 'Error! The "Add to basket" button was not found.'
